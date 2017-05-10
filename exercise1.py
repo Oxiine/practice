3b790654eb3dba7dd3b94535bcda3d504963912e
Ofelia <ofelia.gamboa@hotmail.com>
2017-05-10 18:46:51 -0500

    Hoy aprendi que entre menor cantidad de codigo, hay menos probabilidad de cometer errores, mediante las tablas de la verdad ayuda a comprender el razonamiento entre verdadero y falso... P.S Namorado tem gripe.

--- exercise1.py
+++ exercise1.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-'
 mensagem=raw_input("escreva seu mensagem: ")
 tamanho_de_mensagem=len(mensagem)
-if tamanho_de_mensagem>=140:
-    print("seu twett é muito grande para ser enviado")
-elif tamanho_de_mensagem<5:
-    print "Seu tweet é muito pequeno "+str(tamanho_de_mensagem)+" caracteres para ser enviado"
+if (tamanho_de_mensagem<5)or(tamanho_de_mensagem>=140):
+    print("nao foi enviado, porque é menor que 5 caracteres ou maior que 140 caracteres")
 else:
     print "sucesso seu twett foi enviado"
 print "o tamanho foi de "+str(tamanho_de_mensagem)+" caracteres"
