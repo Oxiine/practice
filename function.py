90c69f0ebe907630242df32aa4ad3ac42983fb31
Ofelia <ofelia.gamboa@hotmail.com>
2017-06-09 18:55:09 -0500

    Hoje aprendí que nao posso escrever ç nem palavras com chapéus, tambem que return, ajuda a simplificar muitas funcoes, quando preciso fazer muitas vezes uma funcao, tambem que def funcoa pra dar entender ao computador que e uma funcao... P.S Chimarrao e chato.

---
+++ function.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-'
+def multiplicacao(numero):
+    return numero * 5
+meu_numero=raw_input("Escreva um numero: ")
+print(multiplicacao(meu_numero))
+outro_numero=raw_input("Escreva outro numero: ")
+print(multiplicacao(outro_numero))
